b6cad437d3a2193dc3300cd15fe28389ca078a6a
daud99 <51257049+daud99@users.noreply.github.com>
2020-09-27 13:28:21 +0500

    error resolved

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 module.exports =  new Sequelize('exam', 'root', '', {
-    host: '25.34.216.242',
+    host: 'localhost',
     dialect: 'mysql',
     pool:{
         max:5,
