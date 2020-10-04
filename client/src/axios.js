b3b31966a7a1e73a135b3269fb89cc46932135bd
omergr8 <62819343+omergr8@users.noreply.github.com>
2020-10-04 23:09:26 +0500

    testing axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3000/api/'
+axios.defaults.baseURL = "http://localhost:3000/api/";
+console.log("hello");
 
-export default axios.create({})
+export default axios.create({});
