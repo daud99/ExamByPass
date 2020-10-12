828708803ba94c294692c5f028146a1fd8798503
omergr8 <umar.mukhtar4455@gmail.com>
2020-10-13 03:33:28 +0500

    changed header for some compoinent and fix some bugs

--- client/src/layout/dashboard/DashboardHeader.vue
+++ client/src/layout/dashboard/DashboardHeader.vue
@@ -1,157 +1,159 @@
 <template>
-   <header class="header-global">
-        <base-nav class="navbar navbar-horizontal navbar-expand-lg navbar-dark bg-primary border-bottom" transparent type="" effect="light" expand>
-            <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-                <img src="img/brand/logovector.svg">
-            </router-link>
+<header class="header-global">
+    <base-nav class="navbar-main" transparent type="" effect="light" expand>
+        <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+            <img src="img/brand/logovector.svg">
+        </router-link>
 
-            <div class="row" slot="content-header" slot-scope="{closeMenu}">
-                <div class="col-6 collapse-brand">
-                    <a href="#">
-                        <img src="img/brand/logovector.svg">
-                    </a>
-                </div>
-                <div class="col-6 collapse-close">
-                    <close-button @click="closeMenu"></close-button>
-                </div>
+        <div class="row" slot="content-header" slot-scope="{closeMenu}">
+            <div class="col-6 collapse-brand">
+                <a href="#">
+                    <img src="img/brand/logovector.svg">
+                </a>
             </div>
+            <div class="col-6 collapse-close">
+                <close-button @click="closeMenu"></close-button>
+            </div>
+        </div>
 
-            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
-                <li class="nav-item pointer">
-                  <h6 
-                    style="color: white; padding-top: 10px;"
-                    @click="h6"
-                  ><strong><router-link style="color: white;" :to="{path: '/contactUs'}" class="nav-item">Contact Us</router-link></strong></h6>
-                </li>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-                <li class="nav-item pointer">
-                  <h6 
-                    style="color: white; padding-top: 10px;"
-                    @click="h6"
-                  ><strong><router-link style="color: white;" :to="{path: '/about'}" class="nav-item">About</router-link></strong></h6>
-                </li>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-                <li class="nav-item pointer">
-                  <h6 
-                    style="color: white; padding-top: 10px;"
-                    @click="h6"
-                  ><strong><router-link style="color: white;" :to="{path: '/faq'}" class="nav-item">FAQS</router-link></strong></h6>
-                </li>
-                &nbsp;
-                &nbsp;
-                &nbsp;
+        <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+            <li class="nav-item pointer">
+                <h6 style="color: white; padding-top: 10px;" @click="h6"><strong>
+                        <router-link style="color: white;" :to="{path: '/contactUs'}" class="nav-item">Contact Us</router-link>
+                    </strong></h6>
+            </li>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <li class="nav-item pointer">
+                <h6 style="color: white; padding-top: 10px;" @click="h6"><strong>
+                        <router-link style="color: white;" :to="{path: '/about'}" class="nav-item">About</router-link>
+                    </strong></h6>
+            </li>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <li class="nav-item pointer">
+                <h6 style="color: white; padding-top: 10px;" @click="h6"><strong>
+                        <router-link style="color: white;" :to="{path: '/faq'}" class="nav-item">FAQS</router-link>
+                    </strong></h6>
+            </li>
+            &nbsp;
+            &nbsp;
+            &nbsp;
 
-                <li class="nav-item pointer">
-                  <h6
-                    style="color: white; padding-top: 10px;"
-                    @click="h6"
-                  ><strong><router-link :to="{path: '/pricing'}" class="nav-item">Pricing</router-link></strong></h6>
-                </li>
-                &nbsp;
-                &nbsp;
-                &nbsp;
-                <li class="nav-item">
-                   <base-dropdown >
-                      <base-button slot="title" type="secondary" icon="fa fa-user">
+            <li class="nav-item pointer">
+                <h6 style="color: white; padding-top: 10px;" @click="h6"><strong>
+                        <router-link :to="{path: '/pricing'}" class="nav-item">Pricing</router-link>
+                    </strong></h6>
+            </li>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <li class="nav-item">
+                <base-dropdown>
+                    <base-button slot="title" type="secondary" icon="fa fa-user">
                         {{
                           _self["auth/getUser"].email
                           }}
-                      </base-button>
-                      <router-link class="dropdown-item" to="/account">
+                    </base-button>
+                    <router-link class="dropdown-item" to="/account">
                         <i class="ni ni-single-02"></i>
                         <span>My Account</span>
-                      </router-link>
+                    </router-link>
 
-                      <!-- <a href="#!" class="dropdown-item">
+                    <!-- <a href="#!" class="dropdown-item">
                         <i class="ni ni-settings-gear-65"></i>
                         <span>Settings</span>
                       </a> -->
-                      <a href="#!" class="dropdown-item">
+                    <a href="#!" class="dropdown-item">
                         <i class="ni ni-single-copy-04"></i>
                         <span>My Exams</span>
-                      </a>
-                      <a href="#!" class="dropdown-item">
+                    </a>
+                    <a href="#!" class="dropdown-item">
                         <i class="ni ni-lock-circle-open"></i>
                         <span>Change Password</span>
-                      </a>
-                      <div class="dropdown-divider"></div>
-                      <a href="#!" class="dropdown-item" @click="logout">
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a href="#!" class="dropdown-item" @click="logout">
                         <i class="ni ni-user-run"></i>
                         <span>Logout</span>
-                      </a>
-                    </base-dropdown>
-                    
+                    </a>
+                </base-dropdown>
 
-                </li>
-            </ul>
-        </base-nav>
-    </header>
+            </li>
+        </ul>
+    </base-nav>
+</header>
 </template>
+
 <script>
 import BaseNav from "@/components/BaseNav";
 import BaseDropdown from "@/components/BaseDropdown";
 import CloseButton from "@/components/CloseButton";
-import {quickRequest} from "../../../common/misc";
-import {mapActions, mapGetters} from 'vuex';
+import {
+    quickRequest
+} from "../../../common/misc";
+import {
+    mapActions,
+    mapGetters
+} from 'vuex';
 
 export default {
-  components: {
-    BaseNav,
-    CloseButton,
-    BaseDropdown
-  },
-  // data() {
-  //   return {
-  //     user: {}
-  //   }
-  // },
-  computed: {
-    ...mapGetters(["auth/getUser"]),
-  },
-  methods: {
-     ...mapActions([
-      'auth/setUser'
-    ]),
-    scrollToElement() {
-      let element_id = document.getElementById("pricingSection");
-      if (element_id) {
-        element_id.scrollIntoView({ block: "end", behavior: "smooth" });
-      }
+    components: {
+        BaseNav,
+        CloseButton,
+        BaseDropdown
     },
-    h6() {
-      this.scrollToElement()
+    // data() {
+    //   return {
+    //     user: {}
+    //   }
+    // },
+    computed: {
+        ...mapGetters(["auth/getUser"]),
     },
-    async logout() {
-      try {
-        let response = await quickRequest("/login/logout", "POST", {});
-        this['auth/setUser']({});
-        localStorage.clear();
-        this.$router.push("login");
-      } 
-      catch(e) {
-        console.log("error");
-        console.log(e);
-      }
+    methods: {
+        ...mapActions([
+            'auth/setUser'
+        ]),
+        scrollToElement() {
+            let element_id = document.getElementById("pricingSection");
+            if (element_id) {
+                element_id.scrollIntoView({
+                    block: "end",
+                    behavior: "smooth"
+                });
+            }
+        },
+        h6() {
+            this.scrollToElement()
+        },
+        async logout() {
+            try {
+                let response = await quickRequest("/login/logout", "POST", {});
+                this['auth/setUser']({});
+                localStorage.clear();
+                this.$router.push("login");
+            } catch (e) {
+                console.log("error");
+                console.log(e);
+            }
+        }
     }
-  }
 };
 </script>
 
 <style scoped>
 .navbar {
-  padding: 1rem 1rem;
+    padding: 1rem 1rem;
 }
 
 @media screen and (min-width: 989px) {
-  .navbar .nav-item {
-      color: white;
-      padding-top: 10px;
-      font-weight: bolder;
-  }
+    .navbar .nav-item {
+        color: white;
+        padding-top: 10px;
+        font-weight: bolder;
+    }
 }
-
 </style>
--- client/src/views/NewComponents/contactUs.vue
+++ client/src/views/NewComponents/contactUs.vue
@@ -1,171 +1,110 @@
 <template>
-  <div class="py-5" id="aboutSection" :style="{marginTop:'100px'}">
+<div class="py-5" id="aboutSection" :style="{marginTop:'100px'}">
     <div class="container-fluid">
         <div class="row">
-          <div class="col offset-lg-2 offset-md-2 col-lg-7 col-md-7 col-xs-12">
-            <h4>Contact Us</h4><br><br>
-            <v-form lazy-validation>
-            <v-text-field
-                v-model="firstname"
-                :error-messages="firstnameErrors"
-                :counter="20"
-                label="First Name"
-                outlined
-                shaped
-                @input="$v.firstname.$touch()"
-                @blur="$v.firstname.$touch()"
-                required
-            ></v-text-field>
-            <v-text-field
-                :error-messages="lastnameErrors"
-                v-model="lastname"
-                :counter="20"
-                label="Last Name"
-                outlined
-                shaped
-                @input="$v.lastname.$touch()"
-                @blur="$v.lastname.$touch()"
-                required
-            ></v-text-field>
+            <div class="col offset-lg-2 offset-md-2 col-lg-7 col-md-7 col-xs-12">
+                <h4>Contact Us</h4><br><br>
+                <v-form lazy-validation>
+                    <v-text-field v-model="firstname" :error-messages="firstnameErrors" :counter="20" label="First Name" outlined shaped @input="$v.firstname.$touch()" @blur="$v.firstname.$touch()" required></v-text-field>
+                    <v-text-field :error-messages="lastnameErrors" v-model="lastname" :counter="20" label="Last Name" outlined shaped @input="$v.lastname.$touch()" @blur="$v.lastname.$touch()" required></v-text-field>
 
-            <v-text-field
-                :error-messages="emailErrors"
-                v-model="email"
-                label="E-mail"
-                outlined
-                shaped
-                @input="$v.email.$touch()"
-                @blur="$v.email.$touch()"
-                required
-            ></v-text-field>
+                    <v-text-field :error-messages="emailErrors" v-model="email" label="E-mail" outlined shaped @input="$v.email.$touch()" @blur="$v.email.$touch()" required></v-text-field>
 
-            <v-text-field
-                :error-messages="problemTypeErrors"
-                v-model="problemType"
-                :counter="20"
-                label="Problem Type"
-                outlined
-                shaped
-                @input="$v.problemType.$touch()"
-                @blur="$v.problemType.$touch()"
-                required
-            ></v-text-field>
+                    <v-text-field :error-messages="problemTypeErrors" v-model="problemType" :counter="20" label="Problem Type" outlined shaped @input="$v.problemType.$touch()" @blur="$v.problemType.$touch()" required></v-text-field>
 
-            <v-textarea
-                :error-messages="messageErrors"
-                v-model="message"
-                :counter="200"
-                label="Message"
-                outlined
-                shaped
-                @input="$v.message.$touch()"
-                @blur="$v.message.$touch()"
-                required
-            ></v-textarea>
+                    <v-textarea :error-messages="messageErrors" v-model="message" :counter="200" label="Message" outlined shaped @input="$v.message.$touch()" @blur="$v.message.$touch()" required></v-textarea>
 
-            <v-btn
-                class="mr-4"
-                >
-                Submit
-            </v-btn>
-            </router-link>
+                    <v-btn class="mr-4">
+                        Submit
+                    </v-btn>
 
-        </v-form>
-          </div>          
+                </v-form>
+            </div>
         </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 import card from '../../components/Card'
-import {mapActions, mapGetters} from 'vuex';
-import { validationMixin } from 'vuelidate'
-import { required, maxLength, minLength, email } from 'vuelidate/lib/validators'
+import {
+    mapActions,
+    mapGetters
+} from 'vuex';
+import {
+    validationMixin
+} from 'vuelidate'
+import {
+    required,
+    maxLength,
+    minLength,
+    email
+} from 'vuelidate/lib/validators'
 
 export default {
-  mixins: [validationMixin],
-  validations: {
-    firstname: { required, maxLength: maxLength(20), minLength: minLength(1) },
-    lastname: { required, maxLength: maxLength(20),minLength: minLength(1) },
-    problemType: { required, maxLength: maxLength(20),minLength: minLength(1) },
-    message: { required, maxLength: maxLength(20),minLength: minLength(1) },
-    email: { required, email }
-  },
-  data: () => ({
-      firstname: '',
-      lastname: '',
-      email: '',
-      problemType:'',
-      message:''
+    mixins: [validationMixin],
+    validations: {
+        firstname: {
+            required,
+            maxLength: maxLength(20),
+            minLength: minLength(1)
+        },
+        lastname: {
+            required,
+            maxLength: maxLength(20),
+            minLength: minLength(1)
+        },
+        problemType: {
+            required,
+            maxLength: maxLength(20),
+            minLength: minLength(1)
+        },
+        message: {
+            required,
+            maxLength: maxLength(20),
+            minLength: minLength(1)
+        },
+        email: {
+            required,
+            email
+        }
+    },
+    data: () => ({
+        firstname: '',
+        lastname: '',
+        email: '',
+        problemType: '',
+        message: ''
     }),
-  components: {
-    card
-  },
-  computed: {
-    ...mapGetters(["auth/getUser"]),
-    firstnameErrors () {
-        const errors = []
-        if (!this.$v.firstname.$dirty) return errors
-        !this.$v.firstname.maxLength && errors.push('First Name must be at most 20 characters long')
-        !this.$v.firstname.minLength && errors.push('First Name must be at least 1 characters long')
-        !this.$v.firstname.required && errors.push('First Name is required.')
-        return errors
+    components: {
+        card
     },
-    lastnameErrors () {
-        const errors = []
-        if (!this.$v.lastname.$dirty) return errors
-        !this.$v.lastname.maxLength && errors.push('Last Name must be at most 20 characters long')
-        !this.$v.lastname.minLength && errors.push('Last Name must be at least 1 characters long')
-        !this.$v.lastname.required && errors.push('Last Name is required.')
-        return errors
+    computed: {
+        ...mapGetters(["auth/getUser"]),
+
     },
-    problemTypeErrors () {
-        const errors = []
-        if (!this.$v.problemType.$dirty) return errors
-        !this.$v.problemType.maxLength && errors.push('Problem Type must be at most 20 characters long')
-        !this.$v.problemType.minLength && errors.push('Problem Type must be at least 1 characters long')
-        !this.$v.problemType.required && errors.push('Problem Type is required.')
-        return errors
+    created() {
+        this.firstname = this["auth/getUser"].firstName;
+        this.lastname = this["auth/getUser"].lastName;
+        this.email = this["auth/getUser"].email;
     },
-    messageErrors () {
-        const errors = []
-        if (!this.$v.message.$dirty) return errors
-        !this.$v.message.maxLength && errors.push('Message must be at most 200 characters long')
-        !this.$v.message.minLength && errors.push('Message must be at least 1 characters long')
-        !this.$v.message.required && errors.push('Message is required.')
-        return errors
+    methods: {
+        submit() {
+            console.log(this.firstname)
+            console.log(this.lastname)
+            console.log(this.email)
+        },
+        clear() {
+            this.$v.$reset()
+            this.firstname = ''
+            this.lastname = ''
+        },
     },
-    emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Must be valid e-mail')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-    }
-  },
-  created() {
-    this.firstname = this["auth/getUser"].firstName;
-    this.lastname = this["auth/getUser"].lastName;
-    this.email = this["auth/getUser"].email;
-  },
-  methods: {
-    submit () {
-        console.log(this.firstname)
-        console.log(this.lastname)
-        console.log(this.email)
-    },
-    clear () {
-        this.$v.$reset()
-        this.firstname = ''
-        this.lastname = ''
-    },
-  },
 }
 </script>
 
 <style scoped>
 .bg-primary {
-  color: white;
+    color: white;
 }
 </style>
