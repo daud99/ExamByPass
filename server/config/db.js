7dafdf3edcb6f64b79e95ea14948ffa87f6470db
Usman Asghar <Usmanchattha199@gmail.com>
2020-11-14 13:04:41 +0500

    changes

--- server/config/db.js
+++ server/config/db.js
@@ -16,26 +16,26 @@ const Sequelize = require("sequelize");
 
 
 //usman's DB configuration
-// module.exports = new Sequelize("exam", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 6000,
-//     idle: 10000,
-//   },
-// })
+module.exports = new Sequelize("exam", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 6000,
+    idle: 10000,
+  },
+})
 
 
 // daud's DB configuration
-module.exports =  new Sequelize('exambypass', 'root', 'toor', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool:{
-        max:5,
-        min:0,
-        acquire:5000,
-        idle:10000
-    },
-});
+// module.exports =  new Sequelize('exambypass', 'root', 'toor', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     pool:{
+//         max:5,
+//         min:0,
+//         acquire:5000,
+//         idle:10000
+//     },
+// });
